0a318b0ab7d9eab6916112cb93ce95bad89a1515
Balazs G. Horvath <bghorvath@pm.me>
2023-05-30 14:39:32 +0200

    add params.yaml

--- acoustic_anomaly_detection/features.py
+++ acoustic_anomaly_detection/features.py
@@ -2,14 +2,17 @@ import torch
 import torch.nn as nn
 import torchaudio
 import numpy as np
+import yaml
+
+params = yaml.safe_load(open("params.yaml"))["features"]
 
 class Spectrogram(nn.Module):
-    def __init__(self, n_fft=400, win_length=400, hop_length=160, power=2.0):
+    def __init__(self):
         super(Spectrogram, self).__init__()
-        self.n_fft = n_fft
-        self.win_length = win_length
-        self.hop_length = hop_length
-        self.power = power
+        self.n_fft = params["n_fft"]
+        self.win_length = params["win_length"]
+        self.hop_length = params["hop_length"]
+        self.power = params["power"]
 
     def forward(self, x):
         x = torchaudio.transforms.Spectrogram(
