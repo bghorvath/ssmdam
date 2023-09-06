5455af0388c1e936328576eddb05b1f0db5c76a5
Balazs G. Horvath <bghorvath@pm.me>
2023-09-06 16:09:36 +0200

    switch from poetry to micromamba

--- easy_setup.sh
+++ easy_setup.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 apt-get update
+apt-get install -y --no-install-recommends \
+    python3.8 python3.8-dev python3-pip python3-setuptools python3-wheel \
+    git make build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev \
+    libsqlite3-dev wget curl llvm libncurses5-dev xz-utils tk-dev libxml2-dev \
+    libxmlsec1-dev libffi-dev liblzma-dev
 
-curl https://pyenv.run | bash
-echo 'eval "$(pyenv init -)"' >> ~/.bashrc
-echo 'eval "$(pyenv virtualenv-init -)"' >> ~/.bashrc
+"${SHELL}" <(curl -L micro.mamba.pm/install.sh)
 source ~/.bashrc
 
-pyenv install
-pyenv local
-pyenv exec pip install --no-cache-dir poetry
-
-poetry install --with gpu
-poetry run dvc pull
+micromamba create -y -f environment.yml
+micromamba activate acoustic-anomaly-detection
+dvc pull
