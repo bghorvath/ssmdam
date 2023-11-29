01e7121e68cc0387cf6ae06a2e891e58dc8ef340
Balazs G. Horvath <bghorvath@pm.me>
2023-11-29 16:52:18 +0100

    fix: easy_install for machines with pytorch-cuda docker image

--- easy_install.sh
+++ easy_install.sh
@@ -1,29 +1,9 @@
 #!/bin/bash
 
 # Install requirements
-apt-get update
-apt-get install -y --no-install-recommends \
-    python3.8 python3.8-dev python3-pip python3-setuptools python3-wheel \
-    git make build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev \
-    libsqlite3-dev wget curl llvm libncurses5-dev xz-utils tk-dev libxml2-dev \
-    libxmlsec1-dev libffi-dev liblzma-dev
 
-# Install micromamba
-"${SHELL}" <(curl -L micro.mamba.pm/install.sh)
-
-export MAMBA_EXE="$HOME/.local/bin/micromamba";
-export MAMBA_ROOT_PREFIX='micromamba';
-__mamba_setup="$("$MAMBA_EXE" shell hook --shell bash --root-prefix "$MAMBA_ROOT_PREFIX" 2> /dev/null)"
-if [ $? -eq 0 ]; then
-    eval "$__mamba_setup"
-else
-    alias micromamba="$MAMBA_EXE"  # Fallback
-fi
-unset __mamba_setup
-
-# Create and activate micromamba environment
-micromamba create -y -f environment.yaml
-micromamba activate acoustic-anomaly-detection
+conda env create -f environment.yaml
+conda activate acoustic-anomaly-detection
 
 # Pull data from dvc remote storage
 dvc pull
